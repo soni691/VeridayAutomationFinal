d25e500a8e54c16bba78c3e81139779b291dc3b0
Administrator <sumit.soni@veriday.com>
2021-04-19 11:36:46 +0000

    import changes

--- src/test/java/com/BlueBird/PageObject/adminHQ/AdminHQ.java
+++ src/test/java/com/BlueBird/PageObject/adminHQ/AdminHQ.java
@@ -1,7 +1,5 @@
 package com.BlueBird.PageObject.adminHQ;
 
-import static org.testng.Assert.assertEquals;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
